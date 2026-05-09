2961418550f888c690cbad90e601ebde6113b745
ebisaa944@ <ebisaa944@gmail.com>
2026-05-09 11:23:45 +0300

    Update backend and frontend source files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q=2#=+q_hrargdi-824qz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
